abdbe7c00bcf4fef5f4544c598ab098dd9f832c8
xhl <root@this.com>
2023-08-22 17:52:16 +0800

    integrate glad and start with a black window

--- main.cpp
+++ main.cpp
@@ -1,57 +1,36 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main() {
-	GLFWwindow *window;
-
 	/* Initialize the library */
 	if (!glfwInit()) {
 		return -1;
 	}
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+	GLFWwindow *window = glfwCreateWindow(640, 480, "你好世界", nullptr, nullptr);
 	if (!window) {
+		std::cout << "Failed to create window\n";
 		glfwTerminate();
 		return -1;
 	}
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
-	/// 根据glew文档，要先创建opengl context才能调用glew API
-	if (glewInit() not_eq GLEW_OK) {
-		std::cout << "Error\n";
-	}
 
-	float positions[]{
-			-1.f, -1.f,
-			0.f, 1.f,
-			1.f, -1.f
-	};
-
-	uint32_t buff;
-	/// hey gl, generate me just 1 buffer and store it into buff
-	glGenBuffers(1, &buff);
-	/// and i am going to use the generated buffer like this:
-	glBindBuffer(GL_ARRAY_BUFFER, buff);
-	/// learn more from http://docs.gl
-	glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window)) {
-		/* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
-		/// draw a triangle (black triangle)
-		glDrawArrays(GL_TRIANGLES, 0, 3);// count: index point(x,y) count
-
-		/* Swap front and back buffers */
-		glfwSwapBuffers(window);
 
 		/* Poll for and process events */
 		glfwPollEvents();
 	}
 
+	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
